0ba558bbbeb06ec82e90461e80b2ecec8d345b04
David Felipe Mora Blanco <David@MacBook-Air-de-Claudia.local>
2019-07-05 22:13:40 -0500

    update

---
+++ definitions.js
@@ -0,0 +1,7 @@
+window.customElements.define('comp-1', SellButton);
+window.customElements.define('comp-2', SellButton2);
+window.customElements.define('comp-3', SellButton3);
+window.customElements.define('comp-4', SellButton4);
+window.customElements.define('comp-5', SellButton5);
+window.customElements.define('comp-6', SellButton6);
+window.customElements.define('comp-7', SellButton7);
