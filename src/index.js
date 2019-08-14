72c6d916a8b38c920a999a7fabc4c964202c4861
Andres <andresbeltranpiloto@hotmail.com>
2019-08-14 12:36:13 -0500

    [update]add card12 and card13 with own css, less comp-1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import Card8 from './components/card8/card8';
 import Card9 from './components/card9/card9';
 import Card10 from './components/card10/card10';
 import Card11 from './components/card11/card11';
+import Card12 from './components/card12/card12';
+import Card13 from './components/card13/card13';
 import Palette from './components/palette/palette';
 
 
@@ -22,6 +24,8 @@ window.customElements.define('comp-8', Card8);
 window.customElements.define('comp-9', Card9);
 window.customElements.define('comp-10', Card10);
 window.customElements.define('comp-11', Card11);
+window.customElements.define('comp-12', Card12);
+window.customElements.define('comp-13', Card13);
 window.customElements.define('my-palette', Palette);
 window.customElements.define('comp-3', Card3);
 window.customElements.define('comp-4', Card4);
